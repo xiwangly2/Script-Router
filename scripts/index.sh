796adb56bc45313fac0107b36bd362b1ae057aa8
xiwangly2 <1334850101@qq.com>
2023-07-19 19:21:09 +0800

    提交更多的功能

--- scripts/index.sh
+++ scripts/index.sh
@@ -2,6 +2,7 @@
 
 # 这里可能需要重新指定URL
 download_url="http://vs8.top/"
+# 推荐使用`bash <(curl -sSL vs8.top)`命令直接执行
 
 # 检查系统是否安装了curl或wget
 if command -v curl >/dev/null 2>&1; then
@@ -15,38 +16,77 @@ else
   exit 1
 fi
 
-echo "请输入选项："
-echo "1. 执行arch.sh脚本"
-echo "2. 执行install.sh脚本"
-echo "3. 执行update.sh脚本"
-echo "4. 执行uninstall.sh脚本"
+function execute_script() {
+  local script_name=$1
+  # 从远程下载脚本并执行
+  $download_command "$script_name" "${download_url}$script_name"
+  chmod +x "$script_name"
+  ./"$script_name"
+}
+
+function show_main_menu() {
+  echo "请输入选项："
+  echo "-1. 执行快捷菜单(一些实用功能)"
+  echo "1. 执行arch.sh脚本(查看架构)"
+  echo "2. 执行install.sh脚本(没写)"
+  echo "3. 执行update.sh脚本(没写)"
+  echo "4. 执行uninstall.sh脚本(没写)"
+  echo "5. 一键换清华源setup_sources.sh(支持多种发行版)"
+}
+
+function show_shortcut_menu() {
+  echo "请输入选项："
+  echo "1. 一键关闭SELinux(redhat系Linux需要)"
+  echo "2. 一键允许root用户连接ssh"
+  echo "3. 一键设置vi-tiny可以使用插入(Debian最小化安装可能出现的问题)"
+}
+
+function execute_shortcut_menu() {
+  local shortcut_choice=$1
+  case $shortcut_choice in
+    1)
+      # 即时生效并永久关闭SELinux的命令
+      sed -i 's/^SELINUX=.*/#&/;s/^SELINUXTYPE=.*/#&/;/SELINUX=.*/a SELINUX=disabled' /etc/sysconfig/selinux && /usr/sbin/setenforce 0
+      ;;
+    2)
+      # 一键允许root用户连接ssh
+      sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config && /etc/init.d/ssh reload
+      ;;
+    3)
+      # 设置vi-tiny可以使用插入
+      sed -i 's/set compatible/set nocompatible/g' /etc/vim/vimrc.tiny
+      ;;
+    *)
+      echo "无效的快捷菜单选项"
+      exit 1
+      ;;
+  esac
+}
+
+show_main_menu
 read choice
 
 case $choice in
+  -1)
+    show_shortcut_menu
+    read shortcut_choice
+    execute_shortcut_menu "$shortcut_choice"
+    ;;
   1)
-    # 从远程下载arch.sh脚本并执行
-    $download_command arch.sh "${download_url}arch.sh"
-    chmod +x arch.sh
-    ./arch.sh
+    execute_script "arch.sh"
     ;;
   2)
-    # 从远程下载install.sh脚本并执行
-    $download_command install.sh "${download_url}install.sh"
-    chmod +x update.sh
-    ./install.sh
+    execute_script "install.sh"
     ;;
   3)
-      # 从远程下载update.sh脚本并执行
-      $download_command update.sh "${download_url}update.sh"
-      chmod +x update.sh
-      ./update.sh
-      ;;
+    execute_script "update.sh"
+    ;;
   4)
-      # 从远程下载uninstall.sh脚本并执行
-      $download_command uninstall.sh "${download_url}uninstall.sh"
-      chmod +x uninstall.sh
-      ./uninstall.sh
-      ;;
+    execute_script "uninstall.sh"
+    ;;
+  5)
+    execute_script "setup_sources.sh"
+    ;;
   *)
     echo "无效的选项"
     exit 1
---
+++ scripts/setup_sources.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+
+# 显示支持的发行版列表
+show_supported_distros() {
+    echo "Supported Linux distributions:(Only the official original version is supported)"
+    echo "1. Ubuntu 22.04 LTS"
+    echo "2. Debian 12"
+    echo "3. CentOS 7"
+    echo "4. Fedora 30"
+    echo "5. OpenBSD"
+    echo "6. Alpine"
+    echo "7. Arch Linux"
+    echo "8. openSUSE Tumbleweed"
+    echo "9. openSUSE Leap 15.3"
+
+    # 添加其他发行版选项...
+}
+
+# 获取用户选择的发行版
+get_distro_choice() {
+    read -p "Enter your Linux distribution choice: " choice
+    echo "$choice"
+}
+
+# 设置软件源
+setup_sources() {
+    local distro=$1
+
+    case $distro in
+        "1")
+            echo "Setting up sources for Ubuntu 22.04 TLS..."
+            cp /etc/apt/sources.list /etc/apt/sources.list.bak
+            sed -i "s/mirrors.tuna.tsinghua.edu.cn/deb.debian.org/g" /etc/apt/sources.list && apt-get update
+            ;;
+        "2")
+            echo "Setting up sources for Debian 12..."
+            cp /etc/apt/sources.list /etc/apt/sources.list.bak
+            sed -i "s/mirrors.tuna.tsinghua.edu.cn/deb.debian.org/g" /etc/apt/sources.list && apt-get update
+            ;;
+        "3")
+            echo "Setting up sources for CentOS 7..."
+            sed -e 's|^mirrorlist=|#mirrorlist=|g' \
+         -e 's|^#baseurl=http://mirror.centos.org/centos|baseurl=https://mirrors.tuna.tsinghua.edu.cn/centos|g' \
+         -i.bak \
+         /etc/yum.repos.d/CentOS-*.repo
+            yum makecache
+            ;;
+        "4")
+            echo "Setting up sources for Fedora 30..."
+            sed -e 's|^metalink=|#metalink=|g' \
+         -e 's|^#baseurl=http://download.example/pub/fedora/linux|baseurl=https://mirrors.tuna.tsinghua.edu.cn/fedora|g' \
+         -i.bak \
+         /etc/yum.repos.d/fedora.repo \
+         /etc/yum.repos.d/fedora-modular.repo \
+         /etc/yum.repos.d/fedora-updates.repo \
+         /etc/yum.repos.d/fedora-updates-modular.repo
+            ;;
+        "5")
+            echo "Setting up sources for OpenBSD..."
+            cp /etc/installurl /etc/installurl.bak
+            echo "https://mirrors.tuna.tsinghua.edu.cn/OpenBSD/" | sudo tee /etc/installurl
+            ;;
+        "6")
+            echo "Setting up sources for Alpine..."
+            cp /etc/apk/repositories /etc/apk/repositories.bak
+            sed -i 's/dl-cdn.alpinelinux.org/mirrors.tuna.tsinghua.edu.cn/g' /etc/apk/repositories
+            ;;
+        "7")
+            echo "Setting up sources for Arch Linux..."
+            cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
+            echo "Server = https://mirrors.tuna.tsinghua.edu.cn/archlinux/\$repo/os/\$arch" | cat - /etc/pacman.d/mirrorlist > temp && mv temp /etc/pacman.d/mirrorlist
+            pacman -Syyu
+            ;;
+        "8")
+            echo "Setting up sources for openSUSE Tumbleweed..."
+            zypper mr -da
+            zypper ar -cfg 'https://mirrors.tuna.tsinghua.edu.cn/opensuse/tumbleweed/repo/oss/' mirror-oss
+            zypper ar -cfg 'https://mirrors.tuna.tsinghua.edu.cn/opensuse/tumbleweed/repo/non-oss/' mirror-non-oss
+            zypper ref
+            ;;
+        "9")
+            echo "Setting up sources for openSUSE Leap 15.3..."
+            zypper mr -da
+            zypper ar -cfg 'https://mirrors.tuna.tsinghua.edu.cn/opensuse/distribution/leap/$releasever/repo/oss/' mirror-oss
+            zypper ar -cfg 'https://mirrors.tuna.tsinghua.edu.cn/opensuse/distribution/leap/$releasever/repo/non-oss/' mirror-non-oss
+            zypper ar -cfg 'https://mirrors.tuna.tsinghua.edu.cn/opensuse/update/leap/$releasever/oss/' mirror-update
+            zypper ar -cfg 'https://mirrors.tuna.tsinghua.edu.cn/opensuse/update/leap/$releasever/non-oss/' mirror-update-non-oss
+            zypper ar -cfg 'https://mirrors.tuna.tsinghua.edu.cn/opensuse/update/leap/$releasever/sle/' mirror-sle-update
+            zypper ar -cfg 'https://mirrors.tuna.tsinghua.edu.cn/opensuse/update/leap/$releasever/backports/' mirror-backports-update
+            ;;
+        *)
+            echo "Unsupported distribution. Exiting..."
+            exit 1
+            ;;
+    esac
+}
+
+# 主函数
+main() {
+    show_supported_distros
+    local distro_choice=$(get_distro_choice)
+
+    setup_sources "$distro_choice"
+
+    echo "Source setup completed."
+}
+
+main
